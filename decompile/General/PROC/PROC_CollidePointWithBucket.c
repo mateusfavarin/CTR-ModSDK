6ca5af5533bba7aff4f94700df75f9f8ae8d8436
mateusfavarin <mateusfavarinc@gmail.com>
2024-08-31 23:42:22 -0300

    Bring collisions to item races

--- decompile/General/PROC/PROC_CollidePointWithBucket.c
+++ decompile/General/PROC/PROC_CollidePointWithBucket.c
@@ -1,20 +1,20 @@
 #include <common.h>
 
+#ifdef USE_ONLINE
+void Online_CollidePointWithBucket(struct Thread* th, short* vec3_pos);
+#endif
+
 void DECOMP_PROC_CollidePointWithBucket(struct Thread* th, short* vec3_pos)
 {
-	struct Thread* other;
-
-// only used with drivers colliding
-// with other drivers, disabled online
 #ifdef USE_ONLINE
-	return;
-#endif
-	
+	Online_CollidePointWithBucket(th, vec3_pos);
+#else
 	while(th != 0)
 	{
 		DECOMP_PROC_CollidePointWithSelf(th, vec3_pos);
-		
+
 		// next
 		th = th->siblingThread;
 	}
+#endif
 }
