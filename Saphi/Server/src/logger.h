4c363dff7fa34474d67cb476ebdcd1dea1e96a12
TheUbMunster <theubmunster@gmail.com>
2024-09-04 13:41:43 -0600

    move logger init to its own file.

---
+++ Saphi/Server/src/logger.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <fmtlog.h>
+#include <filesystem>
+#include <iostream>
+
+namespace Logger
+{
+	//do not call this function, used internally.
+	void LogCallback(int64_t ns, fmtlog::LogLevel level, fmt::string_view location,
+		    size_t basePos, fmt::string_view threadName, fmt::string_view msg, size_t bodyPos,
+			size_t logFilePos)
+	{
+		//since the logfile is set to the log file and not stdout, we use this callback
+	    //to print to stdout.
+		
+	    //printf("%s\n", msg.begin());
+	    std::cout << msg.begin() << std::endl;
+	}
+
+	void LogInit()
+	{
+		fmtlog::setLogLevel(fmtlog::LogLevel::DBG);
+		fmtlog::setThreadName("main");
+		constexpr const char* logFileName = "log.txt", * oldLogFileName = "old-log.txt";
+		//change this to allow variable amounts of saved old log files?
+		//e.g., old-log-1.txt, old-log-2.txt, old-log-3.txt ...
+		if (std::filesystem::exists(oldLogFileName))
+		{
+			try
+			{
+				std::filesystem::remove(oldLogFileName);
+			}
+			catch (...)
+			{
+				logw("Old log file exists, and yet could not delete it");
+			}
+		}
+		if (std::filesystem::exists(logFileName))
+		{
+			try
+			{
+				std::filesystem::rename(logFileName, oldLogFileName);
+			}
+			catch (...)
+			{
+				logw("Previous log file exists, and yet could not rename it to old-log.txt");
+			}
+		}
+		void (*cb)(int64_t ns, fmtlog::LogLevel level, fmt::string_view location, size_t basePos,
+			fmt::string_view threadName, fmt::string_view msg, size_t bodyPos,
+			size_t logFilePos);
+		cb = LogCallback;
+#ifdef _DEBUG
+		fmtlog::setLogLevel(fmtlog::LogLevel::DBG); //includes all logs
+		fmtlog::setLogCB(cb, fmtlog::DBG);
+#else
+		fmtlog::setLogLevel(fmtlog::LogLevel::WRN); //does not include "DBG" or "INF"
+		fmtlog::setLogCB(cb, fmtlog::WRN);
+#endif
+		//you can set a log file *OR* the FILE* for stdout, not both.
+		fmtlog::setLogFile(logFileName);
+		logi("\n   ___                         _       __           ___   \n"
+			"  F __\".   ___ _    _ ___     FJ___    LJ          F __\". \n"
+			" J (___|  F __` L  J '__ J   J  __ `.     ______  J (___| \n"
+			" J\\___ \\ | |--| |  | |--| |  | |--| |  FJ|______| J\\___ \\ \n"
+			".--___) \\F L__J J  F L__J J  F L  J J J  L______J.--___) \\\n"
+			"J\\______J\\____,__LJ  _____/LJ__L  J__LJ__L       J\\______J\n"
+			" J______FJ____,__F|_J_____F |__L  J__||__|        J______F\n"
+			"                  L_J                                     \n"
+			"Saphi Server\nBuild {0} ({1})", __DATE__, __TIME__);
+		fmtlog::startPollingThread(1000); //idk the unit of time, ...milliseconds?
+	}
+}
--- Saphi/Server/src/main.cpp
+++ Saphi/Server/src/main.cpp
@@ -1,19 +1,16 @@
 #include "server.h"
 
+#include "logger.h"
 #include <fmtlog.h>
-#include <filesystem>
-#include <iostream>
-
-static void LogInit();
 
 int main(int argc, char* argv[])
 {
-	LogInit();
+	Logger::LogInit();
 
-	if (argc != 2) 
+	if (argc != 2)
 	{
 		loge("Server arguments missing: [port]");
-		return -1; 
+		return -1;
 	}
 
 	std::istringstream ss(argv[1]);
@@ -31,73 +28,11 @@ int main(int argc, char* argv[])
 	server.Run();
 #else
 	try { server.Run(); }
-	catch (...) {}
+	catch (...)
+	{ loge("server.Run() threw something..."); }
 #endif
 	//server.Run() is a while (true) {}, so this should never be reached currently
 	logi("End of main() reached, server closing...");
 	server.Close();
 	return 0;
-}
-
-static void LogInit()
-{
-	fmtlog::setLogLevel(fmtlog::LogLevel::DBG);
-	fmtlog::setThreadName("main");
-	constexpr const char* logFileName = "log.txt", * oldLogFileName = "old-log.txt";
-	//change this to allow variable amounts of saved old log files?
-	//e.g., old-log-1.txt, old-log-2.txt, old-log-3.txt ...
-	if (std::filesystem::exists(oldLogFileName))
-	{
-		try
-		{
-			std::filesystem::remove(oldLogFileName);
-		}
-		catch (...)
-		{
-			logw("Old log file exists, and yet could not delete it");
-		}
-	}
-	if (std::filesystem::exists(logFileName))
-	{
-		try
-		{
-			std::filesystem::rename(logFileName, oldLogFileName);
-		}
-		catch (...)
-		{
-			logw("Previous log file exists, and yet could not rename it to old-log.txt");
-		}
-	}
-	void (*cb)(int64_t ns, fmtlog::LogLevel level, fmt::string_view location, size_t basePos,
-		fmt::string_view threadName, fmt::string_view msg, size_t bodyPos,
-		size_t logFilePos);
-	cb = [](int64_t ns, fmtlog::LogLevel level, fmt::string_view location, size_t basePos,
-		fmt::string_view threadName, fmt::string_view msg, size_t bodyPos,
-		size_t logFilePos)
-		{
-			//since the logfile is set to the log file and not stdout, we use this callback
-			//to print to stdout.
-
-			//printf("%s\n", msg.begin());
-			std::cout << msg.begin() << std::endl;
-		};
-#ifdef _DEBUG
-	fmtlog::setLogLevel(fmtlog::LogLevel::DBG); //includes all logs
-	fmtlog::setLogCB(cb, fmtlog::DBG);
-#else
-	fmtlog::setLogLevel(fmtlog::LogLevel::WRN); //does not include "DBG" or "INF"
-	fmtlog::setLogCB(cb, fmtlog::WRN);
-#endif
-	//you can set a log file *OR* the FILE* for stdout, not both.
-	fmtlog::setLogFile(logFileName);
-	logi("\n   ___                         _       __           ___   \n"
-		"  F __\".   ___ _    _ ___     FJ___    LJ          F __\". \n"
-		" J (___|  F __` L  J '__ J   J  __ `.     ______  J (___| \n"
-		" J\\___ \\ | |--| |  | |--| |  | |--| |  FJ|______| J\\___ \\ \n"
-		".--___) \\F L__J J  F L__J J  F L  J J J  L______J.--___) \\\n"
-		"J\\______J\\____,__LJ  _____/LJ__L  J__LJ__L       J\\______J\n"
-		" J______FJ____,__F|_J_____F |__L  J__||__|        J______F\n"
-		"                  L_J                                     \n"
-		"Saphi Server\nBuild {0} ({1})", __DATE__, __TIME__);
-	fmtlog::startPollingThread(1000000); //idk the unit of time, ...nanoseconds?
 }
